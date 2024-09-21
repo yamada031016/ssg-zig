e00f32037b158926105bb8e0aa6038ee09671f68
yamada031016 <ymwk.sota@gmail.com>
2024-09-21 23:37:31 +0900

    fix: unspecify layout component in render()

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
+    const lib = b.addStaticLibrary(.{
         .name = "ssg-zig",
         .root_source_file = b.path("src/root.zig"),
         .target = target,
@@ -16,42 +16,35 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = .ReleaseFast,
     });
-    exe.root_module.addImport("ssg-zig", ssg_mod);
+    lib.root_module.addImport("ssg-zig", ssg_mod);
 
-    const run_cmd = b.addRunArtifact(exe);
+    // const run_cmd = b.addRunArtifact(exe);
 
-    run_cmd.step.dependOn(b.getInstallStep());
+    // run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);
+    // }
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    // const run_step = b.step("run", "Run the app");
+    // run_step.dependOn(&run_cmd.step);
 
-    b.installArtifact(exe);
+    b.installArtifact(lib);
 
     const docs_step = b.step("docs", "Emit docs");
     const docs_install = b.addInstallDirectory(.{
-        .source_dir = exe.getEmittedDocs(),
+        .source_dir = lib.getEmittedDocs(),
         .install_dir = .prefix,
         .install_subdir = "docs",
     });
     docs_step.dependOn(&docs_install.step);
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/render.zig
+++ src/render.zig
@@ -4,11 +4,11 @@ const Element = z.element.Element;
 const n = z.node;
 const Node = n.Node;
 
-const RenderError = error {
+const RenderError = error{
     InvalidPageFilePath,
 };
 
-var html_output_path:[]u8 = @constCast("zig-out/html/");
+var html_output_path: []u8 = @constCast("zig-out/html/");
 
 fn generateHtmlFile(id: std.builtin.SourceLocation) !std.fs.File {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -16,36 +16,39 @@ fn generateHtmlFile(id: std.builtin.SourceLocation) !std.fs.File {
     const src = std.fs.path.dirname(id.file) orelse return RenderError.InvalidPageFilePath; // expect "src/pages"
     const parent = std.fs.path.basename(src); // parent dir of the page component file. ex) pages/, about/
 
-    if (std.mem.eql(u8, parent , "pages")) {
+    if (std.mem.eql(u8, parent, "pages")) {
         // single file routing.
         // ex) pages/index.zig, pages/about.zig.
-        if(std.fs.path.dirname(src)) |_| {
+        if (std.fs.path.dirname(src)) |_| {
             const url = std.fs.path.stem(std.fs.path.basename(id.file));
-            return try std.fs.cwd().createFile( try std.fmt.allocPrintZ(allocator, "zig-out/html/{s}.html", .{url}) , .{.read=true});
+            return try std.fs.cwd().createFile(try std.fmt.allocPrintZ(allocator, "zig-out/html/{s}.html", .{url}), .{ .read = true });
         }
     } else if (!std.mem.eql(u8, parent, "src") and std.mem.eql(u8, std.fs.path.basename(id.file), "page.zig")) {
         // multiple file routing.
         // ex) pages/about/page.zig, pages/about/contact/page.zig
         const url = parent;
         var parent_dir = std.fs.path.dirname(src).?;
-        while(!std.mem.eql(u8, std.fs.path.basename(parent_dir), "pages")) {
-            html_output_path = try std.fs.path.join(allocator, &[_][]const u8{html_output_path, std.fs.path.basename(parent_dir)});
-            var output_dir = try std.fs.cwd().makeOpenPath(html_output_path, .{.iterate=true});
+        while (!std.mem.eql(u8, std.fs.path.basename(parent_dir), "pages")) {
+            html_output_path = try std.fs.path.join(allocator, &[_][]const u8{ html_output_path, std.fs.path.basename(parent_dir) });
+            var output_dir = try std.fs.cwd().makeOpenPath(html_output_path, .{ .iterate = true });
             try output_dir.chmod(0o777);
             output_dir.close();
             parent_dir = std.fs.path.dirname(parent_dir).?;
         }
         const fileName = try std.fmt.allocPrintZ(allocator, "{s}.html", .{url});
-        html_output_path = try std.fs.path.join(allocator, &[_][]const u8{html_output_path, fileName});
-        return try std.fs.cwd().createFile(html_output_path, .{.read=true});
+        html_output_path = try std.fs.path.join(allocator, &[_][]const u8{ html_output_path, fileName });
+        return try std.fs.cwd().createFile(html_output_path, .{ .read = true });
     }
     return RenderError.InvalidPageFilePath;
 }
 
 pub fn render(id: std.builtin.SourceLocation, args: Node) !void {
     // std.debug.print("File: {s}\n", .{id.file});
-    const html = generateHtmlFile(id) catch |e| switch(e) {
-        RenderError.InvalidPageFilePath => {std.debug.print("invalid file path: {s}. move below src/pages/**", .{id.file});return;},
+    const html = generateHtmlFile(id) catch |e| switch (e) {
+        RenderError.InvalidPageFilePath => {
+            std.debug.print("invalid file path: {s}. move below src/pages/**", .{id.file});
+            return;
+        },
         else => return,
     };
     try html.chmod(0o777);
@@ -53,20 +56,21 @@ pub fn render(id: std.builtin.SourceLocation, args: Node) !void {
         html.close();
     }
 
-    var root = applicateLayout: {
-        std.fs.cwd().access("src/components/layout.zig", .{}) catch {
-            break :applicateLayout n.createNode(.div).setId("root").init(.{args});
-        };
-        break :applicateLayout n.createNode(.div).setId("root").init(.{@import("components").layout.Layout(args)});
-    };
+    // var root = applicateLayout: {
+    //     std.fs.cwd().access("src/components/layout.zig", .{}) catch {
+    //         break :applicateLayout n.createNode(.div).setId("root").init(.{args});
+    //     };
+    //     // break :applicateLayout n.createNode(.div).setId("root").init(.{@import("components").layout.Layout(args)});
+    // };
+    var root = n.createNode(.div).setId("root").init(.{args});
     var writer = html.writer();
 
     try writer.writeAll("\n<body>");
     try parse(&root, @constCast(&writer));
     try writer.print("</body>", .{});
 
-    const head_file = std.fs.cwd().openFile("zig-out/html/tmp.html", .{.mode = .read_write}) catch {
-        const tmp = try std.fs.cwd().createFile("zig-out/html/tmp.html", .{.read=true});
+    const head_file = std.fs.cwd().openFile("zig-out/html/tmp.html", .{ .mode = .read_write }) catch {
+        const tmp = try std.fs.cwd().createFile("zig-out/html/tmp.html", .{ .read = true });
         const len = try html.getEndPos();
         _ = try std.fs.File.copyRangeAll(html, 0, tmp, 0, len);
         const tmp_len = try tmp.getEndPos();
@@ -100,9 +104,9 @@ fn parse(node: *const Node, writer: *std.fs.File.Writer) !void {
                 },
                 else => {
                     const tag = plane.tag.asText();
-                    if(std.mem.eql(u8, tag, "head")) {
+                    if (std.mem.eql(u8, tag, "head")) {
                         // headタグの内容をtmp.htmlに避難
-                        var head_output = try std.fs.cwd().createFile("zig-out/html/tmp.html", .{.read=true});
+                        var head_output = try std.fs.cwd().createFile("zig-out/html/tmp.html", .{ .read = true });
                         var head_writer = head_output.writer();
                         try head_writer.print("\n<head>", .{});
                         for (node.children.items) |child| {
@@ -110,7 +114,6 @@ fn parse(node: *const Node, writer: *std.fs.File.Writer) !void {
                         }
                         try writer.print("</head>", .{});
                     } else {
-
                         if (node.class) |class| {
                             try writer.print("<{s} class=\"{s}\"", .{ tag, class });
                         } else {
@@ -118,7 +121,7 @@ fn parse(node: *const Node, writer: *std.fs.File.Writer) !void {
                         }
 
                         if (node.id) |_id| {
-                            try writer.print(" id=\"{s}\">", .{ _id });
+                            try writer.print(" id=\"{s}\">", .{_id});
                         } else {
                             try writer.print(">", .{});
                         }
@@ -138,12 +141,12 @@ fn parse(node: *const Node, writer: *std.fs.File.Writer) !void {
         },
         .image => |*image| {
             const src = image.src orelse @panic("Image Element must have image path argument.");
-            try writer.print("<img src=\"{s}\"", .{ src });
-            if(image.alt) |alt| {
-                try writer.print("alt=\"{s}\"", .{ alt });
+            try writer.print("<img src=\"{s}\"", .{src});
+            if (image.alt) |alt| {
+                try writer.print("alt=\"{s}\"", .{alt});
             }
             if (node.class) |class| {
-                try writer.print("class=\"{s}\"", .{class });
+                try writer.print("class=\"{s}\"", .{class});
             }
             if (image.width) |w| {
                 try writer.print("width=\"{}\"", .{w});
@@ -152,19 +155,19 @@ fn parse(node: *const Node, writer: *std.fs.File.Writer) !void {
                 try writer.print("height=\"{}\"", .{h});
             }
             if (node.id) |_id| {
-                try writer.print("id=\"{s}\">", .{ _id });
+                try writer.print("id=\"{s}\">", .{_id});
             } else {
                 try writer.print(">", .{});
             }
         },
         .hyperlink => |*hyperlink| {
             const href = hyperlink.href orelse @panic("HyperLink Element must have hyperlink argument.");
-            try writer.print("<a href=\"{s}\"", .{ href });
+            try writer.print("<a href=\"{s}\"", .{href});
             if (node.class) |class| {
-                try writer.print("class=\"{s}\"", .{ class });
+                try writer.print("class=\"{s}\"", .{class});
             }
             if (node.id) |_id| {
-                try writer.print("id=\"{s}\">", .{ _id });
+                try writer.print("id=\"{s}\">", .{_id});
             } else {
                 try writer.print(">", .{});
             }
@@ -180,9 +183,9 @@ fn parse(node: *const Node, writer: *std.fs.File.Writer) !void {
         },
         .link => |*link| {
             const rel = link.rel orelse @panic("Link Element must have rel and href argument.");
-            try writer.print("<link rel=\"{s}\"", .{ rel });
-            if(link.href) |href| {
-                try writer.print("href=\"{s}\"", .{ href });
+            try writer.print("<link rel=\"{s}\"", .{rel});
+            if (link.href) |href| {
+                try writer.print("href=\"{s}\"", .{href});
             }
             try writer.print(">", .{});
         },
@@ -191,7 +194,7 @@ fn parse(node: *const Node, writer: *std.fs.File.Writer) !void {
             switch (meta_type) {
                 .charset => try writer.print("<meta charset=\"{s}\"", .{meta.charset.?}),
                 .property => try writer.print("<meta property=\"{s}\"content=\"{s}\"", .{ meta.property.?, meta.content.? }),
-                else => try writer.print("<meta name=\"{s}\"content=\"{s}\"", .{ meta_type.asText(),  meta.content.? }),
+                else => try writer.print("<meta name=\"{s}\"content=\"{s}\"", .{ meta_type.asText(), meta.content.? }),
             }
             try writer.print(">", .{});
         },
